2d13ed261829adfdd4e9380b63f81178fb8c134f
lazerdog4 <alexskv4@gmail.com>
2020-06-01 13:54:41 -0700

    added shiny variant for images

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -55,6 +55,8 @@ const PokemonCard: React.FC <PokemonCardProps> = (props) => {
                         <Select onChange = {handleOnChange} value = {pokemonSelected.selectorText} defaultValue = "front_default" variant = "outlined">
                             <MenuItem value = "front_default">Regular front</MenuItem>
                             <MenuItem value = "back_default">Regular back</MenuItem>
+                            <MenuItem value = "front_shiny">Shiny front</MenuItem>
+                            <MenuItem value = "back_shiny">Shiny back</MenuItem>
                         </Select>
                     </CardActions>
                     
