0beb1a8619921a24d65e49ffd67de735c5444ac6
lazerdog4 <alexskv4@gmail.com>
2020-06-23 13:42:23 -0700

    started writing the fetch for the pokemon list

--- src/components/PokemonListView.tsx
+++ src/components/PokemonListView.tsx
@@ -2,6 +2,18 @@ import React from 'react';
 
 
 const PokemonListView: React.FC = () => {
+    
+    var loadPokemons = () => {
+        let url = "https://pokeapi.co/api/v2/pokemon/"
+
+        fetch(url, 
+            {
+                method:"GET",
+                headers:{"origin":"localhost"},
+            })
+            .then(res => {return res.json()})
+    }
+
     return(
         <div>
             hello
